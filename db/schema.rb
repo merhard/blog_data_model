9099c53277f66df30b9aca2fd3010b9bbfb59b6f
Matthew Erhard <merhard@gmail.com>
2013-05-21 21:13:07 -0400

    create post model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130522005457) do
+ActiveRecord::Schema.define(:version => 20130522011220) do
 
   create_table "authors", :force => true do |t|
     t.string   "first_name", :null => false
     t.string   "last_name",  :null => false
     t.string   "email"
     t.string   "twitter"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
